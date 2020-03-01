a243292b2f39cdd75bf058c3fed5b125e491d8a9
ludwj61 <ludwj61@mail.sunysuffolk.edu>
2020-02-29 21:31:34 -0500

    need to fix fnf error

--- app/src/main/java/com/example/periodictable/activities/MainActivity.java
+++ app/src/main/java/com/example/periodictable/activities/MainActivity.java
@@ -5,6 +5,14 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.periodictable.R;
+import com.example.periodictable.model.Element;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +20,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            FileInputStream fis = new FileInputStream(new File("D:\\app-of-elements\\app\\src\\main\\assets\\elements.dat"));
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            ArrayList<?> stuff = (ArrayList<?>) ois.readObject();
+            System.out.println(stuff);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
